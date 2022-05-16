f3af69962f9b08513a40cf0c715a4fff7aff96f4
GuglioIsStupid <bradsbluetab@gmail.com>
2022-05-16 16:11:33 -0400

    change

--- src/love/conf.lua
+++ src/love/conf.lua
@@ -18,9 +18,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ------------------------------------------------------------------------------]]
 
 function love.conf(t)
-	t.identity = "funkin-vasion-rewritten" -- This is for the name of the folder found in %appdata% or .local
+	t.identity = "vanilla-engine" -- This is for the name of the folder found in %appdata% or .local
 	t.version = "11.3" -- You do not need to edit this. This is just for love2d version
 	--t.console = true -- uncomment this to use the console (To use print functions)
 
-	t.window.title = "Friday Night Funkin' Vasion Rewritten v1.5.8" -- Custom window Title
+	t.window.title = "Friday Night Funkin' Vanilla Engine v1.0.0" -- Custom window Title
 end
