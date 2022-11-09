2202304fdacc329db0b4264dc2083bd423119302
GuglioIsStupid <bradsbluetab@gmail.com>
2022-11-09 16:29:58 -0500

    upload a batch file for thingy

--- src/love/main.lua
+++ src/love/main.lua
@@ -53,14 +53,14 @@ Edition: %s
 ]]
 	
 		if selected == 1 then
-		-- Surround traceback in ``` to get a Markdown code block
-		full_error = table.concat({"```",full_error,"```"}, "\n")
-		issuebody = string.format(issuebody, full_error, __VERSION__, love.system.getOS())
-		issuebody = url_encode(issuebody)
-	
-		local subject = string.format("Crash in Vanilla Engine %s", __VERSION__)
-		local url = string.format("https://github.com/VanillaEngineDevs/Vanilla-Engine/issues/new?title=%s&body=%s", subject, issuebody)
-		love.system.openURL(url)
+			-- Surround traceback in ``` to get a Markdown code block
+			full_error = table.concat({"```",full_error,"```"}, "\n")
+			issuebody = string.format(issuebody, full_error, __VERSION__, love.system.getOS())
+			issuebody = url_encode(issuebody)
+		
+			local subject = string.format("Crash in Vanilla Engine %s", __VERSION__)
+			local url = string.format("https://github.com/VanillaEngineDevs/Vanilla-Engine/issues/new?title=%s&body=%s", subject, issuebody)
+			love.system.openURL(url)
 		end
 	end
 end
